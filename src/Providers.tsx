5ba19f2521b46ce6f8c8880564155fff0bb21b19
joshua <joshares9991@gmail.com>
2025-10-06 15:37:53 +0100

    add changes

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "./config/wagmiConfig"; // Adjust path if needed
 import type { ReactNode } from "react";
 import { ToastContainer, Bounce } from "react-toastify";
-import { Analytics } from "@vercel/analytics/next";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -34,7 +33,6 @@ export function Providers({ children }: ProvidersProps) {
           }}
         >
           {children}
-          <Analytics />
           <ToastContainer
             position="top-right"
             autoClose={5000}
