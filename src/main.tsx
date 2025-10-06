b2d50428c967570fec686fe3af71495c9e037939
joshua <joshares9991@gmail.com>
2025-10-06 15:43:55 +0100

    check

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import App from "./App.tsx";
 import { Providers } from "./Providers.tsx";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
